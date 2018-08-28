151316e2ebbc446a7a68aa8284235b7269284279
Kenta Saito <xpenkenta194@gmail.com>
2018-08-28 11:09:33 +0900

    リストに表示するViewを吹き出し型に変更

--- app/src/main/java/com/tokyoc/line_client/MessageAdapter.kt
+++ app/src/main/java/com/tokyoc/line_client/MessageAdapter.kt
@@ -18,7 +18,7 @@ class MessageListAdapter(data: OrderedRealmCollection<Message>) : RealmBaseAdapt
         //val imageview = cell.findViewById<ImageView>(R.id.message_profile_image_view)
         //val bubbleview = cell.findViewById<com.daasuu.bl.BubbleLayout>(R.id.bubble_view)
         //簡単のためリストに格納するViewをsimple_list_item_1にしている
-        val messagecontent = cell.findViewById<TextView>(android.R.id.text1)
+        val messagecontent = cell.findViewById<TextView>(R.id.message_text_view)
     }
 
     //override fun getCount(): Int = messages.size
@@ -31,7 +31,7 @@ class MessageListAdapter(data: OrderedRealmCollection<Message>) : RealmBaseAdapt
         when (convertView) {
             null -> {
                 val inflater = LayoutInflater.from(parent?.context)
-                view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+                view = inflater.inflate(R.layout.view_message, parent, false)
                 viewHolder = ViewHolder(view)
                 view.tag = viewHolder
             }
