0b20f78fbc625df46ff6d4b46ff88cb6b6557944
Kenta Saito <xpenkenta194@gmail.com>
2018-09-02 22:14:47 +0900

    友達追加画面に戻るボタン追加

--- app/src/main/java/com/tokyoc/line_client/AddFriendActivity.kt
+++ app/src/main/java/com/tokyoc/line_client/AddFriendActivity.kt
@@ -12,7 +12,6 @@ import kotlinx.android.synthetic.main.activity_friend_add.*
 class AddFriendActivity: AppCompatActivity() {
 
     private lateinit var realm: Realm
-
     override fun onCreate(saveInstanceState: Bundle?) {
         super.onCreate(saveInstanceState)
         setContentView(R.layout.activity_friend_add)
@@ -20,13 +19,19 @@ class AddFriendActivity: AppCompatActivity() {
 
         val token = intent.getStringExtra("token")
 
-        findViewById<Button>(R.id.to_member).setOnClickListener {
+        findViewById<Button>(R.id.add_friend_button).setOnClickListener {
             realm.executeTransaction {
                 val maxId = realm.where<Member>().max("id")
                 val nextId = (maxId?.toLong() ?: 0L) + 1
                 val member = realm.createObject<Member>(nextId)
                 member.name = user_id.text.toString()
             }
+            val intent = Intent(this, MemberActivity::class.java)
+            intent.putExtra("token", token)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.to_member_button).setOnClickListener {
             val intent = Intent(this, MemberActivity::class.java)
             intent.putExtra("token", token)
             startActivity(intent)
