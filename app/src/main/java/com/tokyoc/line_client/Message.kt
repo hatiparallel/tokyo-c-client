159cbda70b1d973d4061782a1912d6d6c031965f
Hinata Yanagi <hinasssan@gmail.com>
2018-09-10 08:21:21 +0900

    removed Parcelize

--- app/src/main/java/com/tokyoc/line_client/Message.kt
+++ app/src/main/java/com/tokyoc/line_client/Message.kt
@@ -1,14 +1,11 @@
 package com.tokyoc.line_client
 
-import android.os.Parcelable
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
-import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 // MessageデータのFormat
-@Parcelize
-open class Message: Parcelable, RealmObject() {
+open class Message: RealmObject() {
     //データを一意に指定するためにidにPrimaryKeyアノテーションを付加
     @PrimaryKey
     var id: Int = 0
