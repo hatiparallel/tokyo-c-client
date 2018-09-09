56a248dfdcf5e465243c98ac47eefff7d7ee83f2
Hinata Yanagi <hinasssan@gmail.com>
2018-09-10 08:20:20 +0900

    restructured schema

--- app/src/main/java/com/tokyoc/line_client/Member.kt
+++ app/src/main/java/com/tokyoc/line_client/Member.kt
@@ -1,20 +1,21 @@
 package com.tokyoc.line_client
 
-import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
-import kotlinx.android.parcel.Parcelize
+import java.util.*
 
 //MemberデータFormat
-@Parcelize
-open class Member(
+open class Member : RealmObject() {
     @PrimaryKey
-    open var id: Int = 0,
-    @SerializedName("DisplayName")
-    open var name: String = "Aさん",
-    @SerializedName("PhotoURL")
-    open var photo: String = "",
     @SerializedName("UID")
-    open var userId: String = "A"): Parcelable, RealmObject()
+    open var id: String = "A"
 
+    @SerializedName("DisplayName")
+    open var name: String = "Aさん"
+
+    @SerializedName("PhotoURL")
+    open var photo: String = ""
+
+    open var cached: Date = Date(0)
+}
