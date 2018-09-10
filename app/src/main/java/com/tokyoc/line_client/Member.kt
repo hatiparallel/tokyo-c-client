d57564853189a4a7fa8f080cdcdba831a63ef66a
Hinata Yanagi <hinasssan@gmail.com>
2018-09-10 10:18:13 +0900

    implemented Member.lookup

--- app/src/main/java/com/tokyoc/line_client/Member.kt
+++ app/src/main/java/com/tokyoc/line_client/Member.kt
@@ -1,12 +1,45 @@
 package com.tokyoc.line_client
 
 import com.google.gson.annotations.SerializedName
+import io.realm.Realm
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 import java.util.*
 
 //MemberデータFormat
 open class Member : RealmObject() {
+    companion object {
+        fun lookup(uid: String, client: Client): rx.Observable<Member> {
+            return rx.Observable.create<Member> {
+                val realm = Realm.getDefaultInstance()
+                val subscriber = it
+
+                realm.executeTransaction {
+                    var cache = realm.where<Member>().equalTo("id", uid).findFirst()
+                            ?: realm.createObject<Member>(uid)
+
+                    if (Date().getTime() - cache.cached.getTime() <= 5 * 60 * 1000) {
+                        subscriber.onNext(cache)
+                    } else {
+                        client.getPerson(uid).subscribe{
+                            cache = it
+                            cache.cached = Date()
+
+                            realm.insertOrUpdate(cache)
+                            subscriber.onNext(cache)
+                        }
+                    }
+                }
+            }
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+        }
+    }
+
     @PrimaryKey
     @SerializedName("UID")
     open var id: String = "A"
