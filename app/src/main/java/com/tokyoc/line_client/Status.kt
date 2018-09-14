863bdb98d7e423a2c5713719b94be7bf8303cd55
Hinata Yanagi <hinasssan@gmail.com>
2018-09-14 11:41:58 +0900

    added new field friendshipAddedAt

--- app/src/main/java/com/tokyoc/line_client/Status.kt
+++ app/src/main/java/com/tokyoc/line_client/Status.kt
@@ -1,6 +1,9 @@
 package com.tokyoc.line_client
 
+import java.util.*
+
 //StatusデータFormat
 open class Status(
         open var friendshipCount: Int = 0,
+        open var friendshipAddedAt: Date,
         open var latests: List<Summary>)
