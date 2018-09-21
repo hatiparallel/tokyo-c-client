d00f66ca460809f6a4d4e952ddd50bfc338f5573
Hinata Yanagi <hinasssan@gmail.com>
2018-09-21 10:42:33 +0900

    commented

--- app/src/test/java/com/tokyoc/line_client/ServerTest.kt
+++ app/src/test/java/com/tokyoc/line_client/ServerTest.kt
@@ -23,6 +23,8 @@ class ServerTest {
         val client4 = Client.build("TEST_TOKEN4", endpoint)
         val client5 = Client.build("TEST_TOKEN5", endpoint)
 
+
+        // TEST_USER1でPINを発行
         val pinStream = client1.getPIN()
                 .subscribeOn(Schedulers.io())
                 .flatMap {
@@ -45,122 +47,152 @@ class ServerTest {
 
         assertEquals("pin", pinMessage.type)
 
+        // TEST_USER1のPINを用いて、TEST_USER2とTEST_USER3が友達リクエストを送信
         client2.sendPIN(pinMessage.pin).toBlocking().last()
         client3.sendPIN(pinMessage.pin).toBlocking().last()
 
+        // TEST_USER1が(TEST_USER2|TEST_USER3)のリクエストを受信
         var requestMessage = pinStream.next()
 
         assertEquals("request", requestMessage.type)
 
         val requestedBy1 = requestMessage.person
 
+        // TEST_USER1が(TEST_USER2|TEST_USER3)のリクエストを受信
         requestMessage = pinStream.next()
 
         assertEquals("request", requestMessage.type)
 
         val requestedBy2 = requestMessage.person
 
+        // TEST_USER2とTEST_USER3のいずれからもリクエストが来ていることを確認
         assertTrue(setOf("TEST_USER2", "TEST_USER3") == setOf(requestedBy1, requestedBy2))
 
+
+        // TEST_USER2とTEST_USER3からのリクエストを承認
         client1.makeFriends(requestedBy1).toBlocking().last()
         client1.makeFriends(requestedBy2).toBlocking().last()
 
-        var friends1 = client1.getFriends().toBlocking().single()
-        var friends2 = client2.getFriends().toBlocking().single()
-        var friends3 = client3.getFriends().toBlocking().single()
+        // {TEST_USER1,TEST_USER2,TEST_USER3}の友達リストを取得
+        var friends1 = client1.getFriends().toBlocking().single().toSet()
+        var friends2 = client2.getFriends().toBlocking().single().toSet()
+        var friends3 = client3.getFriends().toBlocking().single().toSet()
 
-        assertEquals(2, friends1.size)
-        assertEquals(1, friends2.size)
-        assertEquals(1, friends3.size)
+        // {TEST_USER1,TEST_USER2,TEST_USER3}の友達リストが期待されるものであることを確認
+        assert(friends1 == setOf("TEST_USER2", "TEST_USER3"))
+        assert(friends2 == setOf("TEST_USER1"))
+        assert(friends3 == setOf("TEST_USER1"))
 
-        assert(friends1.contains("TEST_USER2"))
-        assert(friends1.contains("TEST_USER3"))
-
-        assert(friends2.contains("TEST_USER1"))
-        assert(friends3.contains("TEST_USER1"))
-
+        // TEST_USER1がTEST_USER2を初期メンバーとしてグループ"Jesus Christ"を作成
         var group1 = client1.makeGroup(Group(name = "Jesus Christ", members = RealmList("TEST_USER2"))).toBlocking().last()
 
+        // グループ"Jesus Christ"が正しく作成されたことを確認
         assertEquals("Jesus Christ", group1.name)
-        assertEquals(2, group1.members.size)
+        assert(group1.members.toSet() == setOf("TEST_USER1", "TEST_USER2"))
 
-        assert(group1.members.contains("TEST_USER1"))
-        assert(group1.members.contains("TEST_USER2"))
-
+        // TEST_USER2がTEST_USER3をグループ"Jesus Christ"に招待
         client2.invitePerson(group1.id, "TEST_USER3").toBlocking().last()
+
+        // TEST_USER3が{TEST_USER4,TEST_USER5}をグループ"Jesus Christ"に一括招待
         client3.inviteMultiplePerson(group1.id, listOf("TEST_USER4", "TEST_USER5")).toBlocking().last()
 
+        // グループ"Jesus Christ"の情報を再取得
         group1 = client1.getGroup(group1.id).toBlocking().last()
-        assertEquals(5, group1.members.size)
 
-        assert(group1.members.contains("TEST_USER1"))
-        assert(group1.members.contains("TEST_USER2"))
-        assert(group1.members.contains("TEST_USER3"))
-        assert(group1.members.contains("TEST_USER4"))
-        assert(group1.members.contains("TEST_USER5"))
+        // グループ"Jesus Christ"のメンバーを確認
+        assert(group1.members == setOf(
+                "TEST_USER1",
+                "TEST_USER2",
+                "TEST_USER3",
+                "TEST_USER4",
+                "TEST_USER5"))
 
+        // TEST_USER4がグループ"JHVH"の情報を再取得
         var group2 = client4.makeGroup(Group(name = "JHVH", members = RealmList())).toBlocking().last()
+
+        // TEST_USER4がグループ"JHVH"にTEST_USER5を招待
         client4.invitePerson(group2.id, "TEST_USER5").toBlocking().last()
 
+
+        // 各ユーザーのグループ参加状況を確認
         assert(client1.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id))
         assert(client2.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id))
         assert(client3.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id))
         assert(client4.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id, group2.id))
         assert(client5.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id, group2.id))
 
+        // TEST_USER4がTEST_USER5をグループ"JHVH"から退出させる
         client4.leaveGroup(group2.id, "TEST_USER5").toBlocking().last()
 
-        expectError(410) {
+
+        // TEST_USER4が自身をグループ"JHVH"から退出させ、グループは消滅する
+        mustCatch(410) {
             client4.leaveGroup(group2.id, "TEST_USER4").toBlocking().last()
         }
 
+        // 各ユーザーのグループ参加状況を確認
         assert(client1.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id))
         assert(client2.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id))
         assert(client3.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id))
         assert(client4.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id))
         assert(client5.getMemberships().toBlocking().last().map { it.id }.toSet() == setOf(group1.id))
 
+        // TEST_USER1が自身をグループ"Jesus Christ"から退出させる
         client1.leaveGroup(group1.id, "TEST_USER1").toBlocking().last()
+
+        // TEST_USER2が自身をグループ"Jesus Christ"から退出させる
         client2.leaveGroup(group1.id, "TEST_USER2").toBlocking().last()
+
+        // TEST_USER3がTEST_USER4をグループ"Jesus Christ"から退出させる
         client3.leaveGroup(group1.id, "TEST_USER4").toBlocking().last()
 
-        expectError(403) {
+        // TEST_USER4がTEST_USER3をグループ"Jesus Christ"から退出させようとするが、
+        // TEST_USER4はすでにメンバーでなく権限を持たない
+
+        mustCatch(403) {
             client4.leaveGroup(group1.id, "TEST_USER3").toBlocking().last()
         }
 
+        // TEST_USER5が自身をグループ"Jesus Christ"から退出させる
         client5.leaveGroup(group1.id, "TEST_USER5").toBlocking().last()
 
+        // グループ"Jesus Christ"の情報を再取得
         group1 = client3.getGroup(group1.id).toBlocking().last()
-        assertEquals(1, group1.members.size)
-        assert(group1.members.contains("TEST_USER3"))
+        assert(group1.members.toSet() == setOf("TEST_USER3"))
 
-        expectError(410) {
+
+        // TEST_USER3が自身をグループ"Jesus Christ"から退出させ、グループは消滅する
+        mustCatch(410) {
             client3.leaveGroup(group1.id, "TEST_USER3").toBlocking().last()
         }
 
+        // TEST_USER2がTEST_USER1を友達から削除
         client2.deleteFriend("TEST_USER1").toBlocking().last()
 
-        friends1 = client1.getFriends().toBlocking().single()
-        friends2 = client2.getFriends().toBlocking().single()
+        // {TEST_USER1,TEST_USER2}の友達リストを取得
+        friends1 = client1.getFriends().toBlocking().single().toSet()
+        friends2 = client2.getFriends().toBlocking().single().toSet()
 
-        assert(!friends1.contains("TEST_USER2"))
-        assert(friends1.contains("TEST_USER3"))
 
-        assert(!friends2.contains("TEST_USER1"))
+        // {TEST_USER1,TEST_USER2}の友達リストを確認
+        assert(friends1 == setOf("TEST_USER3"))
+        assert(friends2.isEmpty())
 
+        // TEST_USER3がTEST_USER1を友達から削除
         client3.deleteFriend("TEST_USER1").toBlocking().last()
 
-        friends1 = client1.getFriends().toBlocking().single()
-        friends2 = client2.getFriends().toBlocking().single()
-        friends3 = client3.getFriends().toBlocking().single()
+        // {TEST_USER1,TEST_USER2,TEST_USER3}の友達リストを取得
+        friends1 = client1.getFriends().toBlocking().single().toSet()
+        friends2 = client2.getFriends().toBlocking().single().toSet()
+        friends3 = client3.getFriends().toBlocking().single().toSet()
 
-        assert(!friends1.contains("TEST_USER2"))
-        assert(!friends1.contains("TEST_USER3"))
-        assert(!friends2.contains("TEST_USER1"))
-        assert(!friends3.contains("TEST_USER1"))
+        // {TEST_USER1,TEST_USER2}の友達リストを確認
+        assert(friends1.isEmpty())
+        assert(friends2.isEmpty())
+        assert(friends3.isEmpty())
     }
 
-    fun expectError(code: Int, f: () -> Unit) {
+    fun mustCatch(code: Int, f: () -> Unit) {
         try {
             f()
             throw Exception("can't happen")
